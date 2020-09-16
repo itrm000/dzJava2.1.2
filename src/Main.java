b4ec1a4fdb68c627d8efc6f3dbf2fa8a5571d3e5
Anatoly <itrm000@gmail.com>
2020-09-17 01:36:25 +0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long account = 380;
+        long donat = 583;
+        long bonus;
+
+        if (donat > 1000) {
+                bonus = 1;
+        } else {
+            bonus = 0;
+        }
+
+        long sumBonus = bonus * donat / 100;
+        account = account + donat + sumBonus;
+
+        System.out.println(sumBonus);
+        System.out.println(account);
+
+    }
+
+}
