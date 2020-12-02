b22b0314836ec200cbd6b9145aa459f44ffa439c
Anatoly <itrm000@gmail.com>
2020-12-02 21:45:04 +0500

    new donat commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         long account = 380;
-        long donat = 583;
+        long donat = 3252;
         long bonus;
 
         if (donat > 1000) {
