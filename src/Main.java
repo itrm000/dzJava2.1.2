995cccf55a30c4efda2069d585c2a2634a6839a4
Anatoly <itrm000@gmail.com>
2020-12-03 20:40:27 +0500

    without var bonus commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         long account = 380;
-        long donat = 3252;
-        long bonus;
+        long donat = 1675;
+        long sumBonus;
 
         if (donat > 1000) {
-                bonus = 1;
+            sumBonus = 1 * donat / 100;
         } else {
-            bonus = 0;
+            sumBonus = 0;
         }
 
-        long sumBonus = bonus * donat / 100;
+ //       long sumBonus = bonus * donat / 100;
         account = account + donat + sumBonus;
 
         System.out.println(sumBonus);
